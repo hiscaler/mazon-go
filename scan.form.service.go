f77367c5e97a87a8e1cb1bbd46439c692afac0e9
hiscaler <hiscaler@gmail.com>
2025-09-19 15:12:42 +0800

    ...

--- scan.form.service.go
+++ scan.form.service.go
@@ -12,7 +12,7 @@ var ErrInvalidTrackingNumber = errors.New("无效的跟踪号")
 
 type scanFormService service
 
-// Create 基于多个跟踪号生成 ScanForm， 跟踪号必须同一个发货地址才可生成
+// Create 基于多个跟踪号生成 ScanForm，跟踪号必须同一个发货地址才可生成
 // https://www.mazonlabel.com/docs/orderapi/%E7%94%9F%E6%88%90ScanForm%E5%8D%95%E6%8D%AE.html
 func (s scanFormService) Create(ctx context.Context, trackingNumbers ...string) (forms []entity.ScanForm, err error) {
 	numbers := make([]string, 0, len(trackingNumbers))
